1751e41bbf133f8496b5a456beca8deca8181e00
Markus Felten <Markus.Felten@gmx.de>
2019-09-11 16:29:29 +0200

    fix: imports

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,6 +1,30 @@
 <script>
+  import { ActionButton, Menue } from "../../src/index.svelte";
+
+  async function action() {
+    return new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  }
+
+  async function login() {}
+  async function logout() {}
 </script>
 
-<div>
-  <h1>Example</h1>
-</div>
+<nav>
+  <a href="/">Example</a>
+
+  <ul>
+    <li>
+      <Menue>
+        <div slot="title" class="dropdown-trigger">User</div>
+        <ul class="dropdown-content">
+          <li>
+            <a href="#!" on:click|preventDefault={logout}>Logout</a>
+          </li>
+        </ul>
+      </Menue>
+    </li>
+  </ul>
+</nav>
+<main>
+  <ActionButton {action}>Long Running Action</ActionButton>
+</main>
--- src/index.svelte
+++ src/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
-  import Menue from "components/Menue.svelte";
-  import ActionButton from "components/ActionButton.svelte";
+  import Menue from "./components/Menue.svelte";
+  import ActionButton from "./components/ActionButton.svelte";
   import { dateFormatter, formatBytes, formatDuration } from "./util.mjs";
   export { Menue, ActionButton, dateFormatter, formatBytes, formatDuration };
 </script>
