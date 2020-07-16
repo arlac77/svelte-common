8c7dde8c8bcd64a214d06507d06d9b1fe32a1a13
Markus Felten <Markus.Felten@gmx.de>
2020-07-16 23:21:58 +0200

    fix: code cleanup

--- src/components/AnchorContentCatcher.svelte
+++ src/components/AnchorContentCatcher.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onMount, tick } from "svelte";
+  import { onMount } from "svelte";
 
   let a, c;
 
@@ -7,17 +7,17 @@
   let contentElem;
   let pathElem;
 
-onMount(() => {
-  console.log(a.getBoundingClientRect(), c.getBoundingClientRect());
+  onMount(() => {
+    console.log(a.getBoundingClientRect(), c.getBoundingClientRect());
 
-  const anchor = anchorElem.getBBox();
-  const content = contentElem.getBBox();
+    const anchor = anchorElem.getBBox();
+    const content = contentElem.getBBox();
 
-  console.log(anchor, content);
+    console.log(anchor, content);
 
-  pathElem.setAttribute(
-    "d",
-    `M ${anchor.x} ${anchor.y}
+    pathElem.setAttribute(
+      "d",
+      `M ${anchor.x} ${anchor.y}
     Q ${content.x} ${anchor.y}
       ${content.x} ${content.y}
     v ${content.height}
@@ -26,9 +26,8 @@ onMount(() => {
     h ${anchor.width}
     v ${-anchor.height}
     z`
-  );
-});
-
+    );
+  });
 </script>
 
 <div bind:this={a}>
@@ -38,9 +37,28 @@ onMount(() => {
   <slot name="content" />
 </div>
 <svg width="100" height="100" viewBox="0 0 100 100">
-  <rect bind:this={anchorElem} fill="#ddd" y="10" width="100" height="30" stroke="green"
-      stroke-width="5" visibility="visible"/>
-  <rect bind:this={contentElem} fill="#ddd" y="100" width="120" height="200" stroke="green"
-      stroke-width="5" visibility="visible"/>
-  <path bind:this={pathElem} stroke="green" fill="#ddd" stroke-width="5" visibility="visible"/>
+  <rect
+    bind:this={anchorElem}
+    fill="#ddd"
+    y="10"
+    width="100"
+    height="30"
+    stroke="green"
+    stroke-width="5"
+    visibility="visible" />
+  <rect
+    bind:this={contentElem}
+    fill="#ddd"
+    y="100"
+    width="120"
+    height="200"
+    stroke="green"
+    stroke-width="5"
+    visibility="visible" />
+  <path
+    bind:this={pathElem}
+    stroke="green"
+    fill="#ddd"
+    stroke-width="5"
+    visibility="visible" />
 </svg>
