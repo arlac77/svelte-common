c35ae71d7dfcc3d39fb2136db7c11e4dcd404c28
Markus Felten <Markus.Felten@gmx.de>
2020-07-16 22:51:49 +0200

    feat: Modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,33 @@
+<script>
+  let component = null;
+  let props = null;
+
+  let background, wrap;
+
+  function close() {
+    component = null;
+  }
+
+  const handleKeyup = event => {
+    if (event.key === "Escape") {
+      event.preventDefault();
+      close();
+    }
+  };
+
+  const handleOuterClick = event => {
+    if (event.target === background || event.target === wrap) {
+      event.preventDefault();
+      close();
+    }
+  };
+</script>
+
+<svelte:window on:keyup={handleKeyup} />
+
+<div on:click={handleOuterClick} bind:this={background}>
+  <div bind:this={wrap}>
+    <svelte:component this={component} {...props} />
+  </div>
+</div>
+<slot />
--- src/index.svelte
+++ src/index.svelte
@@ -4,6 +4,7 @@
   import Menue from "./components/Menue.svelte";
   import ActionButton from "./components/ActionButton.svelte";
   import Collapse from "./components/Collapse.svelte";
+  import Modal from "./components/Modal.svelte";
   import { dateFormatter, formatBytes, formatDuration, formatDurationISO, formatSecondsSinceEpoch } from "./util.mjs";
-  export { DateTime, Duration, Menue, ActionButton, Collapse, dateFormatter, formatBytes, formatDuration, formatDurationISO, formatSecondsSinceEpoch };
+  export { DateTime, Duration, Menue, ActionButton, Collapse, Modal, dateFormatter, formatBytes, formatDuration, formatDurationISO, formatSecondsSinceEpoch };
 </script>
