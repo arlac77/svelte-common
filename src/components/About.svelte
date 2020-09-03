0f28d12d6a03ce843aa9fb03d2a8f8f6b7d751bf
Markus Felten <Markus.Felten@gmx.de>
2020-09-03 11:26:46 +0200

    feat: About

---
+++ src/components/About.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name;
+  export let version;
+  export let description;    
+</script>
+
+<h2>{name}</h2>
+<p>{description}</p>
+<table class="bordered striped hoverable">
+  <tbody>
+    <tr>
+      <td>Version</td>
+      <td>{version}</td>
+    </tr>
+    <slot/>
+  </tbody>
+</table>
--- src/index.svelte
+++ src/index.svelte
@@ -1,10 +1,11 @@
 <script context="module">
   import Duration from "./components/Duration.svelte";
   import DateTime from "./components/DateTime.svelte";
+  import About from "./components/About.svelte";
   import Menue from "./components/Menue.svelte";
   import ActionButton from "./components/ActionButton.svelte";
   import Collapse from "./components/Collapse.svelte";
   import Modal from "./components/Modal.svelte";
   import { dateFormatter, formatBytes, formatDuration, formatDurationISO, formatSecondsSinceEpoch } from "./util.mjs";
-  export { DateTime, Duration, Menue, ActionButton, Collapse, Modal, dateFormatter, formatBytes, formatDuration, formatDurationISO, formatSecondsSinceEpoch };
+  export { DateTime, Duration, About, Menue, ActionButton, Collapse, Modal, dateFormatter, formatBytes, formatDuration, formatDurationISO, formatSecondsSinceEpoch };
 </script>
