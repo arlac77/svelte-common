80cc33003e3e57c1af492b5e60759f57d0eb59fc
Markus Felten <Markus.Felten@gmx.de>
2020-09-24 14:08:10 +0200

    fix: add optional options arg

--- src/service-worker.mjs
+++ src/service-worker.mjs
@@ -2,28 +2,28 @@ import { readable } from "svelte/store";
 
 /**
  * Create a store holding a service worker
- * @param {string} script 
+ * @param {string} script The URL of the service worker script
+ * @param {Object} options An object containing registration options
  * @return {Readable} store holding the service worker
  */
-export function initializeServiceWorker(script) {
-    const serviceWorker = readable({ state: "unknown" }, set => {
-      navigator.serviceWorker
-        .register(script)
-        .then(serviceWorkerRegistration => {
-          // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/update
-          for (const state of ["active", "waiting", "installing"]) {
-            const sw = serviceWorkerRegistration[state];
-            if (sw) {
-              set(sw);
-              sw.onstatechange = event => set(sw);
-              return;
-            }
+export function initializeServiceWorker(script, options) {
+  const serviceWorker = readable({ state: "unknown" }, set => {
+    navigator.serviceWorker
+      .register(script, options)
+      .then(serviceWorkerRegistration => {
+        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/update
+        for (const state of ["active", "waiting", "installing"]) {
+          const sw = serviceWorkerRegistration[state];
+          if (sw) {
+            set(sw);
+            sw.onstatechange = event => set(sw);
+            return;
           }
-        });
-  
-      return () => {};
-    });
-  
-    return { serviceWorker };
-  }
-  
+        }
+      });
+
+    return () => {};
+  });
+
+  return { serviceWorker };
+}
