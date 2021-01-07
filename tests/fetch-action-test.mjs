a05fb737c248df858880334ed835e09ca5e609dc
Markus Felten <Markus.Felten@gmx.de>
2021-01-07 18:25:34 +0100

    test: fetch-action against mock

--- tests/fetch-action-test.mjs
+++ tests/fetch-action-test.mjs
@@ -1,9 +1,18 @@
 import test from "ava";
 import { FetchAction } from "../src/fetch-action.mjs";
 
-
 globalThis.AbortController = class AbortController {};
-globalThis.fetch = async function(url,options) { return { url }; };
+globalThis.fetch = async function (url, options) {
+  return {
+    url,
+    get statusText() {
+      return url.indexOf("ok") >= 0 ? "ok" : "error";
+    },
+    get ok() {
+      return url.indexOf("ok") >= 0;
+    }
+  };
+};
 
 test("FetchAction", async t => {
   const action = new FetchAction(
@@ -18,4 +27,32 @@ test("FetchAction", async t => {
   t.false(action.canceled);
   t.false(action.completed);
   t.true(action.disabled);
+});
+
+test("FetchAction exec ok", async t => {
+  const action = new FetchAction("https://mydomain.com/ok", {}, {});
+
+  const p = action.start();
+
+  t.true(action.active);
+
+  await p;
+
+  t.false(action.active);
+  t.false(action.failed);
+  t.falsy(action.error);
+});
+
+test("FetchAction exec failed", async t => {
+  const action = new FetchAction("https://mydomain.com/bad", {}, {});
+
+  const p = action.start();
+
+  t.true(action.active);
+
+  await p;
+
+  t.false(action.active);
+  t.true(action.failed);
+  t.truthy(action.error);
 });
