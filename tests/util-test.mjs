ed478c13b9313ce6fc1505fcb4f946d5d9097e24
Markus Felten <Markus.Felten@gmx.de>
2022-07-28 13:10:29 +0200

    test: formatDuration

--- tests/util-test.mjs
+++ tests/util-test.mjs
@@ -1,8 +1,15 @@
 import test from "ava";
-import { formatBytes } from "../src/util.mjs";
+import { formatBytes, formatDuration } from "../src/util.mjs";
 
 test("formatBytes", t => {
   t.is(formatBytes(0), "0 Bytes");
   t.is(formatBytes(1), "1 Byte");
   t.is(formatBytes(10), "10 Bytes");
+});
+
+test("formatDuration", t => {
+  t.is(formatDuration(1), "1s");
+  t.is(formatDuration(60), "1m");
+  t.is(formatDuration(61), "1m 1s");
+  t.is(formatDuration(3661), "1h 1m 1s");
 });
