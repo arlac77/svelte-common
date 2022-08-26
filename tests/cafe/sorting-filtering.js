6b935f44fc8aca59595b3aeabeef416e450cdb46
Markus Felten <Markus.Felten@gmx.de>
2022-08-26 12:39:28 +0200

    test: sorting ...

--- tests/cafe/sorting-filtering.js
+++ tests/cafe/sorting-filtering.js
@@ -7,10 +7,22 @@ fixture`sorting`.page`${base}`;
 test("sorting", async t => {
   const sorta1 = Selector("#sort-a1");
   const a1b = Selector("#a1 > button");
+  const sorta2 = Selector("#sort-a2");
+  const a2b = Selector("#a2 > button");
 
   await t.click(a1b);
   await t.expect(sorta1.value).eql("ascending");
+  await t.expect(sorta2.value).eql("none");
 
   await t.click(a1b);
   await t.expect(sorta1.value).eql("descending");
+  await t.expect(sorta2.value).eql("none");
+
+  await t.click(a1b);
+  await t.expect(sorta1.value).eql("ascending");
+  await t.expect(sorta2.value).eql("none");
+
+  await t.click(a2b);
+  await t.expect(sorta2.value).eql("ascending");
+  await t.expect(sorta1.value).eql("none");
 });
