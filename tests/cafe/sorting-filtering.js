6c37b8ba73c9d2def92f6333180f5ea2f86c4543
Markus Felten <Markus.Felten@gmx.de>
2022-11-30 09:33:57 +0100

    test: more filter values

--- tests/cafe/sorting-filtering.js
+++ tests/cafe/sorting-filtering.js
@@ -11,29 +11,43 @@ test("sorting", async t => {
   const ab = Selector("#a > button");
   const sortb = Selector("#sort-b");
   const bb = Selector("#b > button");
+  const sortc = Selector("#sort-c");
+  const cb = Selector("#c > button");
+  const sorte = Selector("#sort-e");
+  const eb = Selector("#e > button");
 
   await t.expect(sorta.value).eql("ascending");
+  await t.expect(sortb.value).eql("");
+  await t.expect(sortc.value).eql("");
+  await t.expect(sorte.value).eql("");
   await t.expect(a0.innerText).eql("1.1");
 
   await t.click(ab);
   await t.expect(sorta.value).eql("descending");
   await t.expect(sortb.value).eql("");
-  //await t.expect(a0.innerText).eql("5.1");
+  await t.expect(sortc.value).eql("");
+  await t.expect(sorte.value).eql("");
+  await t.expect(a0.innerText).eql("8.1");
 
   await t.click(ab);
   await t.expect(sorta.value).eql("ascending");
   await t.expect(sortb.value).eql("");
+  await t.expect(sortc.value).eql("");
+  await t.expect(sorte.value).eql("");
   await t.expect(a0.innerText).eql("1.1");
 
   await t.click(bb);
   await t.expect(sorta.value).eql("");
   await t.expect(sortb.value).eql("ascending");
-  await t.expect(b0.innerText).eql("");
+  await t.expect(sortc.value).eql("");
+  await t.expect(sorte.value).eql("");
 
   await t.click(bb);
   await t.expect(sorta.value).eql("");
   await t.expect(sortb.value).eql("descending");
-  await t.expect(b0.innerText).contains("1972");
+  await t.expect(sortc.value).eql("");
+  await t.expect(sorte.value).eql("");
+  await t.expect(b0.innerText).contains("1975");
 });
 
 test("filtering", async t => {
