d4c352df416bb4309623839d1499eb681276eee0
Markus Felten <Markus.Felten@gmx.de>
2023-02-02 18:21:13 +0100

    test: no exact dates

--- tests/filter-ava.mjs
+++ tests/filter-ava.mjs
@@ -127,10 +127,10 @@ test(ft, 47, new Date(0), false);
 test(ft, false, new Date(0), false);
 test(ft, /1970/, new Date(0), true);
 test(ft, "1970", new Date(0), true);
-test(ft, "1970-01-01T01:00:00", new Date(0), true);
 test(ft, new Date(0), new Date(0), true);
 
 /*
+test(ft, "1970-01-01T01:00:00", new Date(0), true);
 test(ft, new Date(0), "1970-01-01T00:00:00.000Z", true);
 */
 
