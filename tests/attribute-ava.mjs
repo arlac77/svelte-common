f26ac6f91f964bedc09fb3d2b7ca32d29eb5b430
Markus Felten <Markus.Felten@gmx.de>
2023-09-17 00:32:26 +0100

    test: simplification

--- tests/attribute-ava.mjs
+++ tests/attribute-ava.mjs
@@ -2,17 +2,31 @@ import test from "ava";
 import { tokens } from "../src/attribute.mjs";
 
 function tt(t, input, expected) {
-  const result = [];
-  for(const i of tokens(input)) {
-  	result.push(i);
-  }
-  
-  t.deepEqual(result,expected);
+  t.deepEqual([...tokens(input)], expected);
 }
-tt.title = (providedTitle = "token",input) =>
+
+tt.title = (providedTitle = "token", input) =>
   `${providedTitle} ${input}`.trim();
 
-test(tt, "a< <= >= b>",["a", "<", "<=", ">=", "b", ">"]);
+test(tt, "a< <= >= b>", ["a", "<", "<=", ">=", "b", ">"]);
 
-test(tt, "a123 <= >= a = <> +-[ ] ( ) b.c",
-        ["a123", "<=", ">=","a", "=", "<", ">", "+", "-", "[", "]", "(", ")", "b", ".", "c"]);
+test(tt, "a123 <= >= a = <> +-[]() b.c", [
+  "a123",
+  "<=",
+  ">=",
+  "a",
+  "=",
+  "<",
+  ">",
+  "+",
+  "-",
+  "[",
+  "]",
+  "(",
+  ")",
+  "b",
+  ".",
+  "c"
+]);
+
+test(tt, "a[*].b", ["a", "[", "*", "]", ".", "b"]);
